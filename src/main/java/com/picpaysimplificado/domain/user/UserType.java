cf380e40cf704891dd88c73233766e927dfd30f1
Joao Gabriel Saliba Ribeiro <joao_salibaa@hotmail.com>
2024-02-28 15:57:30 -0300

    feat: imporve UserType

--- src/main/java/com/picpaysimplificado/domain/user/UserType.java
+++ src/main/java/com/picpaysimplificado/domain/user/UserType.java
@@ -1,7 +1,20 @@
 package com.picpaysimplificado.domain.user;
 
-public enum UserType {
-    COMMON,
-    MERCHANT
+import java.util.Arrays;
 
+public enum UserType {
+    COMMON("common"),
+    MERCHANT("merchant");
+
+    private String type;
+
+    UserType(String type) {
+        this.type = type;
+    }
+    public static UserType getUserType(String type) {
+        return  Arrays.stream(UserType.values()).filter(o->o.getType().equalsIgnoreCase(type)).findFirst().orElse(null);
+    }
+    public String getType() {
+        return type;
+    }
 }
