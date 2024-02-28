31b49e08cef6f514f2dce79428fac557ce329ead
Joao Gabriel Saliba Ribeiro <joao_salibaa@hotmail.com>
2024-02-28 16:21:24 -0300

    fix: fix TransationService authorazeTransation

--- src/main/java/com/picpaysimplificado/services/TransationService.java
+++ src/main/java/com/picpaysimplificado/services/TransationService.java
@@ -6,16 +6,15 @@ import com.picpaysimplificado.dtos.TransationDTO;
 import com.picpaysimplificado.repositories.TrasationRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class TransationService {
@@ -32,18 +31,17 @@ public class TransationService {
     @Autowired
     private NotificationService notificationService;
 
-    private final String autorizationEndPoint ="https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc";
+    private final String autorizationEndPoint = "https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc";
 
     @Transactional
     public Transation createTransation(TransationDTO transationDTO) throws Exception {
         User sender = this.userService.findUserById(transationDTO.senderId());
         User receiver = this.userService.findUserById(transationDTO.receiverId());
 
-        userService.validateTrasaction(sender,transationDTO.value());
-        boolean isAuthorized =authorazeTransation(sender,transationDTO.value());
-        if(!isAuthorized){
-            throw  new Exception("Transação não autorizada");
-        }
+        userService.validateTrasaction(sender, transationDTO.value());
+
+        this.authorazeTransation(sender, transationDTO.value());
+
         Transation transation = new Transation();
         transation.setAmount(transationDTO.value());
         transation.setSender(sender);
@@ -56,19 +54,19 @@ public class TransationService {
         trasationRepository.save(transation);
         userService.saveUser(sender);
         userService.saveUser(receiver);
-        this.notificationService.sendNotification(sender,"Transação realizada com sucesso");
-        this.notificationService.sendNotification(receiver,"Transação recebida com sucesso");
-        return  transation;
+        this.notificationService.sendNotification(sender, "Transação realizada com sucesso");
+        this.notificationService.sendNotification(receiver, "Transação recebida com sucesso");
+        return transation;
 
     }
 
-    public boolean authorazeTransation(User sender, BigDecimal value){
-      ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity(this.autorizationEndPoint, Map.class);
-        if(authorizationResponse.getStatusCode() == HttpStatus.OK){
-            String message = (String) authorizationResponse.getBody().get("message");
-            return message.equalsIgnoreCase("Autorizado");
-        }else {
-            return false;
+    public void authorazeTransation(User sender, BigDecimal value) throws Exception {
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity(this.autorizationEndPoint, Map.class);
+        if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) Objects.requireNonNull(authorizationResponse.getBody()).get("message");
+            if (!message.equalsIgnoreCase("Autorizado")) {
+                throw new Exception("Transação não autorizada");
+            }
         }
     }
 }
