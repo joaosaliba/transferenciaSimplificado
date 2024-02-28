e7fb8f2c3ef5cfddb011c5e9441792a0c75adae1
Joao Gabriel Saliba Ribeiro <joao_salibaa@hotmail.com>
2024-02-28 12:22:09 -0300

    feat: create Transation entity

---
+++ src/main/java/com/picpaysimplificado/domain/transation/Transation.java
@@ -0,0 +1,32 @@
+package com.picpaysimplificado.domain.transation;
+
+import com.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transations")
+@Table(name="transations")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+
+}
