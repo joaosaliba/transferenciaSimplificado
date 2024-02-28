a75e58a0ab86ea1c13fc8e0d45bf5fe381cf46ba
Joao Gabriel Saliba Ribeiro <joao_salibaa@hotmail.com>
2024-02-28 14:23:23 -0300

    feat: create UserService

---
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -0,0 +1,32 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTrasaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType().equals(UserType.MERCHANT)) {
+            throw new Exception("Usuário do tipo lojista não autorizado a realizar transação");
+        }
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user) {
+        this.userRepository.save(user);
+    }
+}
